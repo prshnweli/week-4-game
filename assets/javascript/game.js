b98209b94535a3fbd2a37b662c7e25aa691b9752
Prashan Welipitiya <Prashan Welipitiya>
2017-08-12 23:00:54 -0700

    game

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+  <script type="text/javascript">
+
+// create four buttons
+
+// display random number at top between 19 - 120
+
+// add click event to button that adds to variable. 
+
+// button is set to random value between 1-12
+
+// if statement that adds button values. If 
+// goes over top number game loses and resets.
+
+// score at the top changes based on clicking crystals. 
+
+// counter tracks wins and losses
+
+</script
